31a7a91d9a4bd6b4675e124d2092f0ea15ecb74b
NikLessey <nalessey2@gmail.com>
2023-05-12 10:57:56 -0400

    Map 3

--- Game/GamePanel.java
+++ Game/GamePanel.java
@@ -8,11 +8,11 @@ import java.awt.Color;
 import java.awt.geom.Rectangle2D;
 
 /**
-   A component that displays all the game entities
-*/
+ * A component that displays all the game entities
+ */
 
 public class GamePanel extends JPanel
-               implements Runnable {
+        implements Runnable {
 
     // private SoundManager soundManager;
 
@@ -22,19 +22,19 @@ public class GamePanel extends JPanel
     private Thread gameThread;
 
     private BufferedImage image;
-     private Image backgroundImage;
+    private Image backgroundImage;
 
     private volatile boolean isAnimShown;
     private volatile boolean isAnimPaused;
 
     private TileMapManager tileManager;
-    private TileMap    tileMap;
+    private TileMap tileMap;
 
     private boolean levelChange;
     private int level;
     private boolean gameOver;
 
-    public GamePanel () {
+    public GamePanel() {
 
         isRunning = false;
         isPaused = false;
@@ -42,26 +42,24 @@ public class GamePanel extends JPanel
         isAnimPaused = false;
         // soundManager = SoundManager.getInstance();
 
-        image = new BufferedImage (600, 500, BufferedImage.TYPE_INT_RGB);
+        image = new BufferedImage(600, 500, BufferedImage.TYPE_INT_RGB);
 
         level = 1;
         levelChange = false;
     }
 
-
-    public void run () {
+    public void run() {
         try {
             isRunning = true;
             while (isRunning) {
                 if (!isPaused && !gameOver)
                     gameUpdate();
                 gameRender();
-                Thread.sleep (50);    
+                Thread.sleep(50);
             }
+        } catch (InterruptedException e) {
         }
-        catch(InterruptedException e) {}
     }
-    
 
     public void gameUpdate() {
 
@@ -69,34 +67,32 @@ public class GamePanel extends JPanel
 
         if (levelChange) {
             levelChange = false;
-            tileManager = new TileMapManager (this);
+            tileManager = new TileMapManager(this);
 
             try {
                 String filename = "maps/map" + level + ".txt";
-                tileMap = tileManager.loadMap(filename) ;
+                tileMap = tileManager.loadMap(filename);
 
                 int w, h;
                 w = tileMap.getWidth();
                 h = tileMap.getHeight();
 
-                System.out.println ("Changing level to Level " + level);
-                System.out.println ("Width of tilemap " + w);
-                System.out.println ("Height of tilemap " + h);
-            }
-            catch (Exception e) {        // no more maps: terminate game
+                System.out.println("Changing level to Level " + level);
+                System.out.println("Width of tilemap " + w);
+                System.out.println("Height of tilemap " + h);
+            } catch (Exception e) { // no more maps: terminate game
                 gameOver = true;
                 System.out.println(e);
-                System.out.println("Game Over"); 
+                System.out.println("Game Over");
                 return;
-/*
-                System.exit(0);
-*/
+                /*
+                 * System.exit(0);
+                 */
             }
 
             return;
         }
     }
-
 
     public void gameRender() {
 
@@ -104,84 +100,80 @@ public class GamePanel extends JPanel
 
         Graphics2D imageContext = (Graphics2D) image.getGraphics();
 
-        tileMap.draw (imageContext);
+        tileMap.draw(imageContext);
 
         if (gameOver) {
-            Color darken = new Color (0, 0, 0, 125);
-            imageContext.setColor (darken);
-            imageContext.fill (new Rectangle2D.Double (0, 0, 600, 500));
+            Color darken = new Color(0, 0, 0, 125);
+            imageContext.setColor(darken);
+            imageContext.fill(new Rectangle2D.Double(0, 0, 600, 500));
         }
 
-        Graphics2D g2 = (Graphics2D) getGraphics();    // get the graphics context for the panel
+        Graphics2D g2 = (Graphics2D) getGraphics(); // get the graphics context for the panel
         if (g2 != null)
-            g2.drawImage(image, 0, 0, 600, 500, null);    // draw the image on the graphics context
+            g2.drawImage(image, 0, 0, 600, 500, null); // draw the image on the graphics context
 
         imageContext.dispose();
     }
 
-
-    public void startGame() {                // initialise and start the game thread 
+    public void startGame() { // initialise and start the game thread
 
         if (gameThread == null) {
             // soundManager.playSound ("background", true);
 
             gameOver = false;
 
-            tileManager = new TileMapManager (this);
+            tileManager = new TileMapManager(this);
 
             try {
                 tileMap = tileManager.loadMap("maps/map1.txt");
                 int w, h;
                 w = tileMap.getWidth();
                 h = tileMap.getHeight();
-                System.out.println ("Width of tilemap " + w);
-                System.out.println ("Height of tilemap " + h);
+                System.out.println("Width of tilemap " + w);
+                System.out.println("Height of tilemap " + h);
             }
-            
+
             catch (Exception e) {
                 System.out.println(e);
                 System.exit(0);
             }
 
             gameThread = new Thread(this);
-            gameThread.start();            
+            gameThread.start();
 
         }
     }
 
-
-    public void startNewGame() {                // initialise and start a new game thread 
+    public void startNewGame() { // initialise and start a new game thread
         if (gameThread != null || !isRunning) {
-            //soundManager.playSound ("background", true);
+            // soundManager.playSound ("background", true);
 
             endGame();
 
             gameOver = false;
             level = 1;
 
-            tileManager = new TileMapManager (this);
+            tileManager = new TileMapManager(this);
 
             try {
                 tileMap = tileManager.loadMap("maps/map1.txt");
                 int w, h;
                 w = tileMap.getWidth();
                 h = tileMap.getHeight();
-                System.out.println ("Width of tilemap " + w);
-                System.out.println ("Height of tilemap " + h);
-            }
-            catch (Exception e) {
+                System.out.println("Width of tilemap " + w);
+                System.out.println("Height of tilemap " + h);
+            } catch (Exception e) {
                 System.out.println(e);
                 System.exit(0);
             }
 
             gameThread = new Thread(this);
-            gameThread.start();            
+            gameThread.start();
 
         }
     }
 
-
-    public void pauseGame() {                // pause the game (don't update game entities)
+    public void pauseGame() { // pause the game (don't update game entities)
         if (isRunning) {
             if (isPaused)
                 isPaused = false;
@@ -189,42 +181,39 @@ public class GamePanel extends JPanel
                 isPaused = true;
 
             // if (isAnimShown) {
-            //     if (isPaused)
-            //         animation.stopSound();
-            //     else
-            //         animation.playSound();
+            // if (isPaused)
+            // animation.stopSound();
+            // else
+            // animation.playSound();
             // }
         }
     }
 
-
-    public void endGame() {                    // end the game thread
+    public void endGame() { // end the game thread
         isRunning = false;
-        //soundManager.stopClip ("background");
+        // soundManager.stopClip ("background");
     }
 
-    
     public void moveLeft() {
         if (!gameOver)
             tileMap.moveLeft();
     }
-
 
     public void moveRight() {
         if (!gameOver)
             tileMap.moveRight();
     }
 
-    public void shoot(){
-        if(!gameOver)
+    public void shoot() {
+        if (!gameOver)
             tileMap.shoot(true);
     }
 
-    public void interact () {
-        if(!gameOver)
+    public void interact() {
+        if (!gameOver)
             tileMap.interact();
     }
-    
+
     public void jump() {
         if (!gameOver)
             tileMap.jump();
@@ -234,7 +223,7 @@ public class GamePanel extends JPanel
         level += 1;
         levelChange = true;
     }
-    
+
     public void lostLife() {
         level = 1;
         levelChange = true;
